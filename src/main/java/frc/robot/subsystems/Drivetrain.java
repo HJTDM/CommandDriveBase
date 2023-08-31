9868bec6296de8c8455f25d948ac97a850a5c948
Hantao <hantaojin@gmail.com>
2023-08-30 21:48:20 -0500

    Initial Commit

---
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -0,0 +1,46 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.DrivetrainConstants;
+
+public class Drivetrain extends SubsystemBase {
+  private CANSparkMax leftFront = new CANSparkMax(DrivetrainConstants.LEFT_FRONT_ID, MotorType.kBrushless);
+  private CANSparkMax leftBack = new CANSparkMax(DrivetrainConstants.LEFT_BACK_ID, MotorType.kBrushless);
+  private CANSparkMax rightFront = new CANSparkMax(DrivetrainConstants.RIGHT_FRONT_ID, MotorType.kBrushless);
+  private CANSparkMax rightBack = new CANSparkMax(DrivetrainConstants.RIGHT_BACK_ID, MotorType.kBrushless);
+
+  private static final Drivetrain DRIVETRAIN = new Drivetrain();
+  public static Drivetrain getInstance(){
+    return DRIVETRAIN;
+  }
+
+  /** Creates a new Drivetrain. */
+  public Drivetrain() {
+    leftBack.follow(leftFront);
+    rightBack.follow(rightFront);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  public void drive(double throttle, double twist){
+    if(Math.abs(throttle) < 0.1){
+      throttle = 0;
+    }
+    if(Math.abs(twist) < 0.1){
+      twist = 0;
+    }
+
+    leftFront.set(throttle + twist);
+    rightFront.set(throttle - twist);
+  }
+}
